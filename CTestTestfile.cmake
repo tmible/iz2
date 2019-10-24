469563cc734e58dbfe2dd65f24c126663cb1c99a
tmible <52622566+tmible@users.noreply.github.com>
2019-10-24 17:52:08 +0300

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nikita/Tехнопарк/1 семестр/Углублённое программирование на С С++/iz2
-# Build directory: /home/nikita/Tехнопарк/1 семестр/Углублённое программирование на С С++/iz2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
